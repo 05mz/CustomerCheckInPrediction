74c1ac2eeeae21be4343516ad2a53be956046bfb
Zaid Mohammad <74952827+05mz@users.noreply.github.com>
2022-08-17 19:48:52 +0530

    Update checkinPrediction.py

--- checkinPrediction.py
+++ checkinPrediction.py
@@ -60,9 +60,9 @@ history = classifier.fit(features_train, labels_train, epochs=20, validation_dat
 prediction_features = classifier.predict(features_test)
 performance = classifier.evaluate(features_test, labels_test)
 st.write("performance",performance)
-
+st.write("")
 #testing the test data
-
+st.write("FOR THE TEST DATA")
 test_df = pd.read_csv('test.csv')
 
 labelsTest = test_df['BookingsCheckedIn']
@@ -73,6 +73,7 @@ labelsTest.replace(not 0,1,inplace=True)
 
 featuresTest = pd.get_dummies(featuresTest)
 featuresTest[0:5]
+st.write("")
 
 featuresTest = featuresTest.values.astype('float32')
 labelsTest = labelsTest.values.astype('float32')
@@ -107,14 +108,17 @@ chart_data1 = pd.read_csv(
      'training.csv', usecols =["Age", "LodgingRevenue"])
 st.bar_chart(chart_data1, use_container_width=True)
 st.write("graph shows the relationship between age groups and lodging revenue \n incase of surplus, we need to prioritize the middle aged people in 40s \n as they spend more on the booking for amenties")
+st.write("")
 
 chart_data2 = pd.read_csv(
      'training.csv', usecols =["BookingsCheckedIn","Age"])
 st.bar_chart(chart_data2,use_container_width=True)
 st.write("graph shows that people in 20s and 30s are prone to not checking in after the booking")
+st.write("")
 
 
 chart_data3 = pd.read_csv(
      'training.csv', usecols =["DistributionChannel"])
 st.bar_chart(chart_data3,use_container_width=True)
 st.write("above graph shows that a lot of people come through travel agents \n so we should focus on advertising ourselves through travel agents")
+st.write("")
