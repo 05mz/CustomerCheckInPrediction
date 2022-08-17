8c418cad89321db867deacf6abb2b6222be9fce1
Zaid Mohammad <74952827+05mz@users.noreply.github.com>
2022-08-17 14:32:14 +0530

    Update checkinPrediction.py

--- checkinPrediction.py
+++ checkinPrediction.py
@@ -9,10 +9,12 @@ from sklearn.model_selection import train_test_split
 #importing dataset
 training_df = pd.read_csv('training.csv')
 
-# training_df.head(5)
-
-# training_df.shape
-# training_df.describe()
+print("training_df.head(5)")
+training_df.head(5)
+print("training_df.shape")
+training_df.shape
+print("training_df.describe()")
+training_df.describe()
 
 #creating labels and features
 labels = training_df['BookingsCheckedIn']
@@ -21,16 +23,18 @@ features = training_df.drop(columns=['BookingsCheckedIn'])
 #data preprocessing
 labels.replace(0,0,inplace=True)
 labels.replace(not 0,1,inplace=True)
-
+print("Labels")
+labels[0:5]
 #data preprocessing, here we replace textual data with random numeric data
 features = pd.get_dummies(features)
+print("features")
 features[0:5]
 
 features = features.values.astype('float32')
 labels = labels.values.astype('float32')
-# print(features[0:2])
-# print(labels[0:2])
-print(len(features[0]))
+print("features after preprocessing",features[0:2])
+print("labels after preprocessing",labels[0:2])
+print("lenght of features",len(features[0]))
 
 #splitting training and testing and validation data
 features_train, features_test, labels_train, labels_test=train_test_split(features,labels,test_size=0.2)
@@ -54,7 +58,7 @@ history = classifier.fit(features_train, labels_train, epochs=20, validation_dat
 #evaluating the model
 prediction_features = classifier.predict(features_test)
 performance = classifier.evaluate(features_test, labels_test)
-print(performance)
+print("performance",performance)
 
 #testing the test data
 
@@ -92,31 +96,30 @@ prediction_features = classifier.predict(features_test1)
 performance1 = classifier.evaluate(features_test1, labels_test1)
 print(performance1)
 
-x=training_df['Age']
-y=training_df['LodgingRevenue']
-plt.figure(1 , figsize = (15 ,6))
-plt.bar(x,y)
-plt.xlabel('Age') , plt.ylabel('LodgingRevenue')
-plt.show()
+def graph1():
+  x=training_df['Age']
+  y=training_df['LodgingRevenue']
+  plt.figure(1 , figsize = (15 ,6))
+  plt.bar(x,y)
+  plt.xlabel('Age') , plt.ylabel('LodgingRevenue')
+  plt.show()
+print(graph1)
+print("graph shows the relationship between age groups and lodging revenue \n incase of surplus, we need to prioritize the middle aged people in 40s \n as they spend more on the booking for amenties")
 
-#graph shows the relationship between age groups and lodging revenue
-#incase of surplus, we need to prioritize the middle aged people in 40s
-#as they spend more on the booking for amenties
-
-
-x=training_df['Age']
-y=training_df['BookingsCheckedIn']
-plt.figure(1 , figsize = (15 ,6))
-plt.bar(x,y)
-plt.xlabel('Age') , plt.ylabel('BookingsCheckedIn')
-plt.show()
-
-#graph shows that people in 20s and 30s are prone to not checking in after the booking
+def graph2():
+  x=training_df['Age']
+  y=training_df['BookingsCheckedIn']
+  plt.figure(1 , figsize = (15 ,6))
+  plt.bar(x,y)
+  plt.xlabel('Age') , plt.ylabel('BookingsCheckedIn')
+  plt.show()
+print(graph2)
+print("graph shows that people in 20s and 30s are prone to not checking in after the booking")
 
 import seaborn
-plt.figure(1 , figsize = (15 , 5))
-seaborn.countplot(y = 'DistributionChannel' , data = pd.read_csv('training.csv'))
-plt.show()
-
-#above graph shows that a lot of people come through travel agents
-#so we should focus on advertising ourselves through travel agents
+def graph3():
+  plt.figure(1 , figsize = (15 , 5))
+  seaborn.countplot(y = 'DistributionChannel' , data = pd.read_csv('training.csv'))
+  plt.show()
+print(graph3)
+print("above graph shows that a lot of people come through travel agents \n so we should focus on advertising ourselves through travel agents")
