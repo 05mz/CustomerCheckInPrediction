e2a60c58debbd1c8acb6e6132f5b4aa28540d723
Zaid Mohammad <74952827+05mz@users.noreply.github.com>
2022-08-17 15:02:17 +0530

    Update checkinPrediction.py

--- checkinPrediction.py
+++ checkinPrediction.py
@@ -104,7 +104,7 @@ def graph1():
   plt.bar(x,y)
   plt.xlabel('Age') , plt.ylabel('LodgingRevenue')
   plt.show()
-st.write(graph1())
+st.image(graph1())
 st.write("graph shows the relationship between age groups and lodging revenue \n incase of surplus, we need to prioritize the middle aged people in 40s \n as they spend more on the booking for amenties")
 
 def graph2():
@@ -114,7 +114,7 @@ def graph2():
   plt.bar(x,y)
   plt.xlabel('Age') , plt.ylabel('BookingsCheckedIn')
   plt.show()
-st.write(graph2())
+st.image(graph2())
 st.write("graph shows that people in 20s and 30s are prone to not checking in after the booking")
 
 import seaborn
@@ -122,5 +122,5 @@ def graph3():
   plt.figure(1 , figsize = (15 , 5))
   seaborn.countplot(y = 'DistributionChannel' , data = pd.read_csv('training.csv'))
   plt.show()
-st.write(graph3())
+st.image(graph3())
 st.write("above graph shows that a lot of people come through travel agents \n so we should focus on advertising ourselves through travel agents")
